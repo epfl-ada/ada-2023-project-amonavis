cd4bcee90e3e63725ad733e8abdab7b772d43ec1
Nicolas Perez <56159408+WhimZig@users.noreply.github.com>
2023-11-09 16:29:13 +0100

    Data readers for the dataset

---
+++ data_readers.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import numpy as np
+import networkx
+
+def read_wikispeedia_graph() -> networkx.Graph:
+    wikispeedia = networkx.read_edgelist('datasets/wikispeedia_paths-and-graph/links.tsv',
+                                         create_using=networkx.DiGraph)
+    return wikispeedia
+
+def read_finished_paths() -> pd.DataFrame:
+    paths_finished = pd.read_csv('datasets/wikispeedia_paths-and-graph/paths_finished.tsv', sep='\t', skiprows=15,
+                                 names=['hashedIpAddress', 'timestamp', "durationInSec", 'path', "rating"])
+    return paths_finished
+
+def read_unfinished_paths() -> pd.DataFrame:
+    paths_unfinished = pd.read_csv('datasets/wikispeedia_paths-and-graph/paths_unfinished.tsv', sep='\t', skiprows=16,
+                                   names=['hashedIpAddress', 'timestamp', "durationInSec", 'path', "target", "type"])
+
+    return paths_unfinished
+
+def read_shortest_path_df() -> pd.DataFrame:
+    """Reads in the shortest path matrix. In this method, if there is no path between two
+    nodes then the matrix returns a -1"""
+    shortest_path = np.genfromtxt("datasets/wikispeedia_paths-and-graph/shortest-path-distance-matrix.txt",
+                                  delimiter=1, missing_values=-1, dtype=int)
+    articles = pd.read_csv('datasets/wikispeedia_paths-and-graph/articles.tsv', sep='\t', skiprows=12,
+                           names=["article_name"])
+
+    shortest_path_df = pd.DataFrame(shortest_path, index=articles.values, columns=articles.values)
+    return shortest_path_df
+
+def plaintext_article_finder(article_name: str) -> str:
+    art_file_name = "datasets/plaintext_articles/" + article_name + ".txt"
+    text_file = open(art_file_name, "r", encoding="utf8")
+    res_string = text_file.read()
+    text_file.close()
+
+    return res_string
